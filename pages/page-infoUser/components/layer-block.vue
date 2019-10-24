8e908de3791e96d52885e775cfc2582886b7f51f
KujoMiva <kujomiva@foxmail.com>
2019-10-24 19:42:33 +0800

    添加用户信息详情的组件

---
+++ pages/page-infoUser/components/layer-block.vue
@@ -0,0 +1,30 @@
+<template>
+  <view class="container-layer-block">
+    <!--  pageStart -->
+    <view class="item-title">{{ titie }}</view>
+    <slot>插槽内容</slot>
+    <view class="item-btn-more">更多{{ moreText }}</view>
+    <!-- pageEnd -->
+  </view>
+</template>
+
+<script>
+export default {
+  props: {
+    titie: {
+      type: String,
+      default: '默认标题'
+    },
+    moreText: {
+      type: String,
+      default: '内容'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container-layer-block {
+  position: relative;
+}
+</style>
